0088a9bf3222c5237ad21a1cc3a9622a6540d6c5
Ben5 <ben-smith@blueyonder.co.uk>
2013-07-21 08:14:18 +0000

    styling the navbar

--- site/views/nav.php
+++ site/views/nav.php
@@ -1,20 +1,16 @@
-<div style='float:left;'>
-    <h1><?php echo 'Picroll'; ?></h1>
-</div>
-
-<div style='float: right; text-align: right;'>
+<div class="navbarWrapper">
     <ul>
-    <li>
-        <a href='/picroll/html/upload/index'>Upload Pictures</a>
-    </li>
-    <li>
-        <a href='/picroll/html/view/viewallimages'>View Uploaded Pictures</a>
-    </li>
     <li>
         <a href='/picroll/html/login/logout'>Log out</a>
     </li>
     <li>
         <a href='/picroll/html/friends/index'>Friends</a>
+    </li>
+    <li>
+        <a href='/picroll/html/view/viewallimages'>View Uploaded Pictures</a>
+    </li>
+    <li>
+        <a href='/picroll/html/upload/index'>Upload Pictures</a>
     </li>
     </ul>
 </div>
