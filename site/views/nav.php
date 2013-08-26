304ddc680acd1e45e82c27facb3853bc95e569d8
Ben5 <ben-smith@blueyonder.co.uk>
2013-08-26 11:13:16 +0000

    Restyled navbar to use bootstrap style

--- site/views/nav.php
+++ site/views/nav.php
@@ -1,22 +1,28 @@
-<div class="navbarWrapper">
-    <div class="navTitle">
-        <span><?php echo $navbarTitle; ?></span>
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand"><?php echo $navbarTitle; ?></a>
     </div>
 
-    <ul>
-    <li>
-        <a href='/picroll/html/login/logout'>Log out</a>
-    </li>
-    <li>
-        <a href='/picroll/html/friends/index'>Friends</a>
-    </li>
-    <li>
-        <a href='/picroll/html/view/viewallimages'>View Uploaded Pictures</a>
-    </li>
-    <li>
-        <a href='/picroll/html/upload/index'>Upload Pictures</a>
-    </li>
-    </ul>
-</div>
-
-<div style='clear: both'></div>
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href='/picroll/html/upload/index'>Upload Pictures</a>
+            </li>
+            <li>
+                <a href='/picroll/html/view/viewallimages'>View Uploaded Pictures</a>
+            </li>
+            <li>
+                <a href='/picroll/html/friends/index'>Friends</a>
+            </li>
+            <li>
+                <a href='/picroll/html/login/logout'>Log out</a>
+            </li>
+        </ul>
+    </div>
+</nav>
