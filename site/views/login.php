43e988aea26a6e8a8c5a1676a72351db7f87ae47
Ben5 <ben-smith@blueyonder.co.uk>
2013-08-26 20:23:08 +0000

    converting login page to bootstrap styling, with responsive layout and better accessibility

--- site/views/login.php
+++ site/views/login.php
@@ -1,25 +1,47 @@
-<h3>Login</h3>
+<div class="row">
+    <div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-1">
+        <div class="section well">
+            <h3>Sign In</h3>
 
-<div class='section left'>
-    <h3>Sign In</h3>
+            <form name="SignInForm" action="LogInToAccount" method="POST" role="form">
+                <div class="form-group">
+                    <label for="username"> Username </label>
+                    <input type="text" class="form-control" name="username" id="username"/>
+                </div>
+                <div class="form-group">
+                    <label for="password"> Password </label>
+                    <input type="password" class="form-control" name="password" id="password"/>
+                </div>
 
-    <form name='SignInForm' action='LogInToAccount' method='POST'>
-        Username:           <br><input type='text'     name='username'/>    <br><br>
-        Password:           <br><input type='password' name='password'/>    <br><br>
+                <input type="submit" class="btn btn-primary" value="Sign In">
+            </form>
+        </div>
+    </div>
 
-        <input type='submit' value='Sign In'>
-    </form>
-</div>
+    <div class="col-xs-12 col-sm-5 col-md-4 col-md-offset-2">
+        <div class="section well">
+            <h3>Create Account</h3>
 
-<div class='section right'>
-    <h3>Create Account</h3>
+            <form name="CreateAccountForm" action="CreateAccount" method="POST" role="form">
+                <div class="form-group">
+                    <label for="newUsername"> Username </label>
+                    <input type="text" class="form-control" name="username" id="newUsername"/>
+                </div>
+                <div class="form-group">
+                    <label for="newPassword"> Password </label>
+                    <input type="password" class="form-control" name="password" id="newPassword"/>  
+                </div>
+                <div class="form-group">
+                    <label for="repassword"> Re-enter Password </label>
+                    <input type="password" class="form-control" name="repassword" id="repassword"/>
+                </div>
+                <div class="form-group">
+                    <label for="email"> Email Address </label>
+                    <input type="email" class="form-control" name="email" id="email"/>
+                </div>
 
-    <form name='CreateAccountForm' action='CreateAccount' method='POST'>
-        Username:           <br><input type='text'     name='username'/>    <br><br>
-        Password:           <br><input type='password' name='password'/>    <br><br>
-        Re-enter Password:  <br><input type='password' name='repassword'/>  <br><br>
-        Email Address:      <br><input type='email'    name='email'/>       <br><br>
-
-        <input type='submit' value='Create Account'>
-    </form>
+                <input type="submit" class="btn btn-primary" value="Create Account">
+            </form>
+        </div>
+    </div>
 </div>
