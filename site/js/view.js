caac6f859f23941cc1252173dd6ccf386cd94fce
Ben5 <ben-smith@blueyonder.co.uk>
2013-10-12 07:18:48 +0000

    Thumbnails are now clickable to view full version. Can also select multiple images with checkbox feedback, after clicking 'select images' button.

--- site/js/view.js
+++ site/js/view.js
@@ -11,9 +11,57 @@ $(document).ready(function() {
     // Event Handler Registration
     //
 
+    // Select Images button click
+    $('#btnSelectPictures').on('click', ToggleSelectMode);
+
+    // Toggle Picture Select State
+    $('img.thumbnail').on('click', ImageClickHandler);
+
+    // Toggle the select state of images if the user clicks the overlay directly, not the image
+    $('div.overlay').on('click', function() {ToggleSelectState($(this).siblings('img'));})
+
+    // Click on the full size image overlay backdrop
+    $('#fullscreenImage').on('click', HideFullscreenImage);
+    $('#fullscreenImage').on('click', 'img',  function(event) {event.stopPropagation();}); // we dont want to call HideFullscreenImage if we click the image itself
+
+
     //
     // Event Handlers
     //
+
+    // Toggle the Select Pictures mode
+    function ToggleSelectMode() {
+        var $overlays = $('div.overlay');
+        $overlays.toggle(200);
+        $overlays.find('span.glyphicon-check').hide();
+        $overlays.find('span.glyphicon-unchecked').show();
+    }
+
+    // Handle clicks on thumbnails
+    function ImageClickHandler() {
+        if ($('div.overlay').is(':visible')) {
+            ToggleSelectState($(this));
+        } else {
+            // Show the full screen image!
+            var fullImageSrc = $(this).data('fullimgsrc');
+            var fullImage = new Image();
+            fullImage.src = fullImageSrc;
+            $('#fullscreenImageWrapper').fadeIn(400);
+            $('#fullscreenImage').empty().append(fullImage);
+
+            // add a faded background
+            $('div.modal-backdrop').fadeIn(400)
+        }
+    }
+
+    // Hide Fullscreen Image
+    function HideFullscreenImage() {
+        // hide the image
+        $('#fullscreenImageWrapper').fadeOut(400);
+
+        // hide the overlay
+        $('div.modal-backdrop').fadeOut(400);
+    }
 
     //
     // Callbacks
@@ -22,4 +70,9 @@ $(document).ready(function() {
     //
     // Helpers
     //
+
+    // Toggle the Select State of pictures
+    function ToggleSelectState(thisImg) {
+        thisImg.siblings('div.overlay').find('span').each(function() { $(this).toggle(); });
+    }
 });
--- site/views/view.php
+++ site/views/view.php
@@ -5,6 +5,19 @@
 </div>
 
 <div class="container">
+    <div class="col-xs-12 well well-sm">
+        <h5>Edit Pictures</h5>
+        <div class="btn-group">
+            <button id="btnSelectPictures" class="btn btn-default">Select Pictures</button>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-default" disabled="disabled">Delete <span class="glyphicon glyphicon-trash"></span></button>
+            <button class="btn btn-default" disabled="disabled">Add To Album <span class="glyphicon glyphicon-book"></span></button>
+        </div>
+    </div>
+</div>
+
+<div class="container">
     <?php
         $countSmall  = 0;
         $countMed    = 0;
@@ -18,12 +31,18 @@
             $countSmall++;
             $countMed++;
             $countLarge++;
+
+            $thumbImg = $imageBase.$imageName.'-thumb'.$imageExt;
+            $fullImg  = $imageBase.$imageName.$imageExt;
     ?>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
-                <img src='<?php echo $imageBase.$imageName.$imageExt;?>' class="thumbnail img-thumbnail"/>
+                <img src="<?php echo $thumbImg;?>" class="thumbnail img-thumbnail" data-fullimgsrc="<?php echo $fullImg;?>"/>
+                <div class="overlay">
+                    <span class="glyphicon glyphicon-check" style="display:none"></span>
+                    <span class="glyphicon glyphicon-unchecked"></span>
+                </div>
             </div>
     <?php
-
             if ($countSmall === $perRowSmall) {
                 echo '<div class="clearfix visible-sm"></div>';
                 $countSmall = 0;
@@ -38,4 +57,12 @@
             }
         }
     ?>
+</div>
+
+<div id="fullscreenImageWrapper">
+    <div id="fullscreenImage">
+    </div>
+</div>
+
+<div class="modal-backdrop">
 </div>
